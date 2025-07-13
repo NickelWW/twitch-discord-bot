80ab7f2f4c0fc593a543ee50dd8d63b4965a5877
NickelW <nickelworldplayz@gmail.com>
2025-07-14 09:40:42 +1200

    Added a windows run script

---
+++ run_bot_windows.bat
@@ -0,0 +1,4 @@
+@echo off
+python bot.py
+:: remove pause if you want the command prompt to disappear automatically
+pause
--- run_twitch_bot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# this script will only work on linux, of course
-cd cd to where the python file is
-exec python bot.py
